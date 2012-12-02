923256a823770a1ecd4dc60de5a8a62ab39482f6
Ryan Woodring <rdwoodring@gmail.com>
2012-12-02 10:10:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoSite::Application.config.secret_token = '19908f0a470c1891107649f1494d2c41a9ff3ff66cdfa4f355f042258e03fdd00f91b8ea9bf30c20c11dc977b00d44e5a18b76af4dae2a5c0c171c4030892908'
