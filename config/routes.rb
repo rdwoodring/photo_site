293d74f25edf84494bf25d4ba12c201782ac5ef9
Ryan Woodring <rdwoodring@gmail.com>
2012-12-02 17:55:41 -0500

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 PhotoSite::Application.routes.draw do
+  
   root to: 'static_pages#home'
+  
   match '/about', to: 'static_pages#about'
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
