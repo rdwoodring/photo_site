175a8fb4507804557b627428fdf35d425ddb9131
Ryan Woodring <rdwoodring@gmail.com>
2012-12-22 16:42:22 -0500

    add image category to db so that images can be filtered by category

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,17 +2,19 @@
 #
 # Table name: pictures
 #
-#  id          :integer          not null, primary key
-#  title       :string(255)
-#  description :text
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id            :integer          not null, primary key
+#  title         :string(255)
+#  description   :text
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  image         :string(255)
+#  imagecategory :string(255)
 #
 
 class Picture < ActiveRecord::Base
-	attr_accessible :title, :description, :image
+	attr_accessible :title, :description, :image, :imagecategory
 	
-	validates_presence_of :title, :description
+	validates_presence_of :title, :description, :imagecategory
 	
 	mount_uploader :image, ImageUploader
 end
