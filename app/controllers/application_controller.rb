aae3673b38b660e65a0fc67ce05eb618f4be4411
Ryan Woodring <rdwoodring@gmail.com>
2013-02-11 21:56:16 -0500

    fix http_basic so it plays nice with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
+  
   protect_from_forgery
   
-  #this line just password protects the whole site.
-  #don't forget to remove it once you get sessions set up for admin users and are ready
-  #to really push the site live
-  # http_basic_authenticate_with name: "ryan", password: "testingtesting"
+  def authenticate
+	  #this line just password protects the whole site.
+	  #don't forget to remove it once you get sessions set up for admin users and are ready
+	  #to really push the site live
+	  authenticate_or_request_with_http_basic do |username, password|
+		username == "ryan" && password == "testingtesting"
+	  end
+	warden.custom_failure! if performed?
+   end
 end
