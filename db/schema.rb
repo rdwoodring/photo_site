8626887c5241b4a21ad2b2d6413765ac3a0776fc
Ryan Woodring <rdwoodring@gmail.com>
2013-02-12 23:17:53 -0500

    finish user authorization. still need to handle access denied exceptions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130203171958) do
+ActiveRecord::Schema.define(:version => 20130213040449) do
 
   create_table "news_items", :force => true do |t|
     t.text     "newsbody"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20130203171958) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
